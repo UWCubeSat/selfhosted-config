64e7913a871232963d4f0ac04759729f27c1f57e
Mark Polyakov <mark@markasoftware.com>
2022-02-01 17:43:07 -0800

    Enable fail2ban.
    Server was having horrible latency today. Fail2ban is always what I try first before debugging more.

--- nathan/go-networking.sh
+++ nathan/go-networking.sh
@@ -7,7 +7,7 @@ echo
 source remotely.sh
 remotely_go
 
-remotely apt-get install -y rsync wireguard bridge-utils
+remotely apt-get install -y rsync wireguard bridge-utils fail2ban
 
 upload /etc/wireguard -p --chmod 600
 upload /etc/iptables
@@ -23,6 +23,8 @@ remotely systemctl daemon-reload
 remotely systemctl reload wg-quick@wg0 || true
 remotely systemctl start wg-quick@wg0 iptables-custom
 remotely systemctl enable wg-quick@wg0 iptables-custom
+remotely systemctl start fail2ban
+remotely systemctl enable fail2ban
 
 echo
 echo 'DONE with networking'
